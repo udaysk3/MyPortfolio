8006f9e80b173aad63cfd62f012ac98c236e5306
udaysk3 <burluudaysantoshkumar3@gmail.com>
2024-09-12 20:31:13 +0530

    Hero photo updated

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -64,13 +64,13 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero.png"
+              src="/images/uday_bg.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
+              width={500}
+              height={500}
             />
           </div>
         </motion.div>
